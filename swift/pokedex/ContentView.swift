bd4da821aec7dabef6d3056c8502bb90c7c29d39
Cem Bıçakcı <cmbicakci@gmail.com>
2023-12-02 12:12:40 +0300

    swift file

---
+++ swift/pokedex/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  pokedex
+//
+//  Created by Cem Bıçakcı on 2.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift/pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Cem Bıçakcı on 2.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
